f6e33f17c71f02891931399f7c4d7be1bdd5d1f6
andyvorld <andyvorld@live.com.au>
2022-09-03 12:01:55 +1000

    added missing namespace

--- LGSTrayGUI/MainWindow.xaml.cs
+++ LGSTrayGUI/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.Versioning;
 using System.Text;
 using System.Threading;
