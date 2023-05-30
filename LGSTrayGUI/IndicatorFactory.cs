6f8f477f3c5084ec55191d084722327487b388f0
Vortex <vortex@dobko.eu>
2023-05-30 22:43:58 +0200

    removed unnessesary usings

--- LGSTrayGUI/IndicatorFactory.cs
+++ LGSTrayGUI/IndicatorFactory.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace LGSTrayGUI
 {
