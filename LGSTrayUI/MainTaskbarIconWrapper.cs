0d94b884d25c3ccb9b2dfbd76af5ceacca5958ca
andyvorld <andyvorld@live.com.au>
2024-01-11 00:47:16 +1100

    bugfix/fix-duped-icon

--- LGSTrayUI/MainTaskbarIconWrapper.cs
+++ LGSTrayUI/MainTaskbarIconWrapper.cs
@@ -1,58 +1,72 @@
 ﻿using Hardcodet.Wpf.TaskbarNotification;
 using System;
-using System.ComponentModel;
 using System.Windows;
 
-namespace LGSTrayUI
+namespace LGSTrayUI;
+
+public class MainTaskBarIcon : TaskbarIcon
 {
-    public class MainTaskbarIconWrapper : IDisposable
+    public MainTaskBarIcon() : base()
     {
-        #region IDisposable
-        private bool disposedValue;
+        ContextMenu = (System.Windows.Controls.ContextMenu) Application.Current.FindResource("SysTrayMenu");
+        BatteryIconDrawing.DrawUnknown(this);
+    }
+}
 
-        protected virtual void Dispose(bool disposing)
+public class MainTaskbarIconWrapper : IDisposable
+{
+    #region IDisposable
+    private bool disposedValue;
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
         {
-            if (!disposedValue)
+            if (disposing)
             {
-                if (disposing)
-                {
-                    _taskbarIcon.Dispose();
-                    LogiDeviceIcon.RefCountChanged -= OnRefCountChanged;
-                }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
-                // TODO: set large fields to null
-                disposedValue = true;
+                _taskbarIcon?.Dispose();
+                LogiDeviceIcon.RefCountChanged -= OnRefCountChanged;
             }
+
+            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            // TODO: set large fields to null
+            disposedValue = true;
         }
+    }
 
-        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-        // ~MainTaskbarIconWrapper()
-        // {
-        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-        //     Dispose(disposing: false);
-        // }
+    // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+    // ~MainTaskbarIconWrapper()
+    // {
+    //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+    //     Dispose(disposing: false);
+    // }
 
-        public void Dispose()
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+    #endregion
+
+    private TaskbarIcon? _taskbarIcon = new MainTaskBarIcon();
+
+    public MainTaskbarIconWrapper()
+    {
+        LogiDeviceIcon.RefCountChanged += OnRefCountChanged;
+        OnRefCountChanged(LogiDeviceIcon.RefCount);
+    }
+
+    private void OnRefCountChanged(int refCount)
+    {
+        if (refCount == 0)
         {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
+            _taskbarIcon ??= new MainTaskBarIcon();
         }
-        #endregion
-
-        private readonly TaskbarIcon _taskbarIcon = (TaskbarIcon)Application.Current.FindResource("NotifyIcon");
-
-        public MainTaskbarIconWrapper()
+        else
         {
-            BatteryIconDrawing.DrawUnknown(_taskbarIcon);
-            LogiDeviceIcon.RefCountChanged += OnRefCountChanged;
-            OnRefCountChanged(LogiDeviceIcon.RefCount);
-        }
-
-        private void OnRefCountChanged(int refCount)
-        {
-            _taskbarIcon.Visibility = (refCount == 0) ? Visibility.Visible : Visibility.Collapsed;
+            _taskbarIcon?.Dispose();
+            _taskbarIcon = null;
         }
     }
 }
