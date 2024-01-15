70d81c136915fef721248f8682364011b2a32c34
andyvorld <andyvorld@live.com.au>
2024-01-15 22:12:17 +1100

    bugfix/update-webserver-settings

--- LGSTrayPrimitives/AppSettings.cs
+++ LGSTrayPrimitives/AppSettings.cs
@@ -1,40 +1,47 @@
-﻿namespace LGSTrayPrimitives
+﻿namespace LGSTrayPrimitives;
+
+public class AppSettings
 {
-    public class AppSettings
+    public UISettings UI { get; set; } = null!;
+
+    public HttpServerSettings HTTPServer { get; set; } = null!;
+
+    public IDeviceManagerSettings GHub { get; set; } = null!;
+
+    public NativeDeviceManagerSettings Native { get; set; } = null!;
+}
+
+public class UISettings
+{
+    public bool EnableRichToolTips { get; set; }
+}
+
+public class HttpServerSettings
+{
+    public bool Enabled { get; set; }
+    public int Port { get; set; }
+
+    private string _addr = null!;
+    public string Addr
     {
-        public UISettings UI { get; set; } = null!;
-
-        public HttpServerSettings HTTPServer { get; set; } = null!;
-
-        public IDeviceManagerSettings GHub { get; set; } = null!;
-
-        public NativeDeviceManagerSettings Native { get; set; } = null!;
+        get => _addr;
+        set => _addr = (value == "0.0.0.0") ? "+" : value;
     }
 
-    public class UISettings
-    {
-        public bool EnableRichToolTips { get; set; }
-    }
+    public bool UseIpv6 { get; set; }
 
-    public class HttpServerSettings
-    {
-        public bool Enabled { get; set; }
-        public int Port { get; set; }
-        public string Addr { get; set; } = null!;
+    public string UrlPrefix => $"http://{Addr}:{Port}";
+}
 
-        public string UrlPrefix => $"http://{Addr}:{Port}/";
-    }
+public class IDeviceManagerSettings
+{
+    public bool Enabled { get; set; }
+}
 
-    public class IDeviceManagerSettings
-    {
-        public bool Enabled { get; set; }
-    }
+public class NativeDeviceManagerSettings : IDeviceManagerSettings
+{
+    public int RetryTime { get; set; } = 10;
+    public int PollPeriod { get; set; } = 600;
 
-    public class NativeDeviceManagerSettings : IDeviceManagerSettings
-    {
-        public int RetryTime { get; set; } = 10;
-        public int PollPeriod { get; set; } = 600;
-
-        public IEnumerable<string> DisabledDevices { get; set; } = [];
-    }
+    public IEnumerable<string> DisabledDevices { get; set; } = [];
 }
